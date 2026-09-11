438559099bdecca19cedefd292036ee774d2ca93
Ewan <contact@ewanc.uk>
2026-09-11 16:52:47 +0200

    Added backgrounds to the buttons, making them more buttony. Stopped buttons from being pressed when a bezier point is selected.

--- src/main.rs
+++ src/main.rs
@@ -39,7 +39,7 @@ fn main() {
         vec![100.0, 100.0],
         vec![400.0, 100.0],
         vec![100.0, 300.0],
-        vec![400.0, 300.0]
+        vec![400.0, 300.0],
     ];
 
     let mut selected_circle: i32 = -1;
@@ -49,18 +49,19 @@ fn main() {
     let control_line_res = 200;
 
     // Declaring buttons
-    let arr_buttons: Vec<(f32, f32, String)> = vec![
-        (WIDTH as f32 - 107.0, 10.0, String::from("add point")),
-        (WIDTH as f32 - 107.0, 20.0, String::from("remove point"))
+    let arr_buttons: Vec<(f32, f32, String, f32)> = vec![
+        (WIDTH as f32 - 107.0, 10.0, String::from("add point"), 3.0),
+        (
+            WIDTH as f32 - 107.0,
+            30.0,
+            String::from("remove point"),
+            3.0,
+        ),
     ];
 
     let mut button_pressed: bool = false;
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        // for i in buffer.iter_mut() {
-        //     *i = 22; // write something more funny here!
-        // }
-
         // gets the mouse position
         window.get_mouse_pos(MouseMode::Clamp).map(|mouse| {
             mouse_pos[0] = mouse.0 as f32;
@@ -123,22 +124,33 @@ fn main() {
             }
 
             // Detecting button presses, should be moved into a loop in the future
-            if is_pt_in_rect(
-                    mouse_pos.clone(), 
-                    vec![arr_buttons[0].0.clone(), arr_buttons[0].1.clone()], 
-                    vec![(arr_buttons[0].2.len() * font::GLYPH_WIDTH-1) as f32, font::GLYPH_HEIGHT as f32]) {          
-                if button_pressed == false {
-                    let mut new_pt: Vec<Vec<f32>> = vec![vec![10.0, 10.0]];
-                    circle_pos_arr.append(&mut new_pt);
-                    button_pressed = true;
-                }
-            } else if is_pt_in_rect(
-                    mouse_pos.clone(), 
-                    vec![arr_buttons[1].0.clone(), arr_buttons[1].1.clone()], 
-                    vec![(arr_buttons[1].2.len() * font::GLYPH_WIDTH-1) as f32, font::GLYPH_HEIGHT as f32]) {
-                if button_pressed == false {
-                    circle_pos_arr.pop();
-                    button_pressed = true;
+            // Only allows the buttons to be pressed when a bezier point is not selected
+            if selected_circle == -1 {
+                if is_pt_in_rect(
+                    mouse_pos.clone(),
+                    vec![arr_buttons[0].0.clone(), arr_buttons[0].1.clone()],
+                    vec![
+                        (arr_buttons[0].2.len() * font::GLYPH_WIDTH - 1) as f32,
+                        font::GLYPH_HEIGHT as f32,
+                    ],
+                ) {
+                    if button_pressed == false {
+                        let mut new_pt: Vec<Vec<f32>> = vec![vec![10.0, 10.0]];
+                        circle_pos_arr.append(&mut new_pt);
+                        button_pressed = true;
+                    }
+                } else if is_pt_in_rect(
+                    mouse_pos.clone(),
+                    vec![arr_buttons[1].0.clone(), arr_buttons[1].1.clone()],
+                    vec![
+                        (arr_buttons[1].2.len() * font::GLYPH_WIDTH - 1) as f32,
+                        font::GLYPH_HEIGHT as f32,
+                    ],
+                ) {
+                    if button_pressed == false {
+                        circle_pos_arr.pop();
+                        button_pressed = true;
+                    }
                 }
             }
 
@@ -175,8 +187,22 @@ fn main() {
         // GUI Buttons
         for button in &arr_buttons {
             let text = button.2.clone();
-            let mut text_x_offset = button.0.clone()  as usize;
-            let text_y_offset = button.1.clone()  as usize;
+            let mut text_x_offset = button.0.clone() as usize;
+            let text_y_offset = button.1.clone() as usize;
+
+            for x in (-1 * button.3 as i32)
+                ..((button.2.len() * font::GLYPH_WIDTH) as i32 + button.3 as i32)
+            {
+                for y in (-1 * button.3 as i32)..(font::GLYPH_HEIGHT as i32 + button.3 as i32) {
+                    if (x + text_x_offset as i32) < WIDTH as i32
+                        && (y + text_y_offset as i32) < HEIGHT as i32
+                    {
+                        let index =
+                            ((y + text_y_offset as i32) * WIDTH as i32) + x + text_x_offset as i32;
+                        buffer[index as usize] = button_bg_colour;
+                    }
+                }
+            }
 
             for c in text.chars() {
                 let letter: [[i32; 7]; 9] = get_letter_glyph(c);
@@ -188,18 +214,13 @@ fn main() {
                                 let index = ((y + text_y_offset) * WIDTH) + x + text_x_offset;
                                 buffer[index] = red_color;
                             }
-                        } else {
-                            if x + text_x_offset < WIDTH && y + text_y_offset < HEIGHT {
-                                let index = ((y + text_y_offset) * WIDTH) + x + text_x_offset;
-                                buffer[index] = button_bg_colour;
-                            }
                         }
                     }
                 }
                 text_x_offset += 7;
             }
         }
-        
+
         // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
         window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
     }
@@ -228,7 +249,7 @@ fn get_pt_of_nth_degree_bezier(t: &f32, arr_pts: &Vec<Vec<f32>>) -> Vec<f32> {
     let n = arr_pts.len() - 1;
 
     for i in 0..arr_pts.len() {
-        let basis= binomial_coefficient(n as u128, i as u128) as f64
+        let basis = binomial_coefficient(n as u128, i as u128) as f64
             * (1.0 - t).powf((n - i) as f32) as f64
             * (t.powf(i as f32)) as f64;
         v_return[0] += (basis * arr_pts[i][0] as f64) as f32;
@@ -305,12 +326,12 @@ fn get_letter_glyph(c: char) -> [[i32; 7]; 9] {
 }
 
 fn is_pt_in_rect(pt: Vec<f32>, rect_pos: Vec<f32>, rect_dims: Vec<f32>) -> bool {
-    for x in rect_pos[0] as i32..(rect_dims[0]+rect_pos[0]) as i32 {
-        for y in rect_pos[1] as i32..(rect_dims[1]+rect_pos[1]) as i32 {
+    for x in rect_pos[0] as i32..(rect_dims[0] + rect_pos[0]) as i32 {
+        for y in rect_pos[1] as i32..(rect_dims[1] + rect_pos[1]) as i32 {
             if pt == vec![x as f32, y as f32] {
                 return true;
             }
         }
     }
-    return false
+    return false;
 }
